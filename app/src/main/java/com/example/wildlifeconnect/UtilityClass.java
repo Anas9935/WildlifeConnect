586ec4e71d60ce9b720bcc4905c33ac08737aa87
Anas Amin <aaansari9999@gmail.com>
2019-11-16 17:17:35 +0530

    Second Commit

---
+++ app/src/main/java/com/example/wildlifeconnect/UtilityClass.java
@@ -0,0 +1,12 @@
+package com.example.wildlifeconnect;
+
+public class UtilityClass {
+
+    public static String getdate(long timestamp){
+        return "12-10-15";
+    }
+    public static  String getLocn(float lat,float lon){
+        return "New Delhi";
+    }
+
+}
